27ecdd927d3bb774d4e3abaee25b626c39d94e56
Dyam Marcano <dyam.marcano@gmail.com>
2016-08-11 03:34:09 -0400

    changes

---
+++ locales/en.js
@@ -0,0 +1,11 @@
+{
+	"Database maintenance": "Database maintenance",
+	"Create new database": "Create new database",
+	"database name": "database name",
+	"Create": "Create",
+	"Delete database": "Delete database",
+	"Delete": "Delete",
+	"Connection statistics": "Connection statistics",
+	"Storage": "Storage",
+	"Documents": "Documents"
+}
